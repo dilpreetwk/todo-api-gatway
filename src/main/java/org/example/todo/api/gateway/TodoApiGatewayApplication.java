e9de2c6b694bfa3f51123dbcdd7d0c2fda2fd50e
Sahney <Dilpreet.Sahney@wolterskluwer.com>
2025-08-04 14:06:23 +0530

    initial commit

---
+++ src/main/java/org/example/todo/api/gateway/TodoApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package org.example.todo.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TodoApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/todo/api/gateway/TodoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todo.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
